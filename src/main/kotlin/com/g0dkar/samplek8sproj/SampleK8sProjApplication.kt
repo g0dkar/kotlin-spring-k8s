132c209703fdc4fde11d67f52a38ea2d7e37e996
Rafael Lins <rafael.lins@n26.com>
2019-11-28 15:59:14 +0100

    Spring Starter files

---
+++ src/main/kotlin/com/g0dkar/samplek8sproj/SampleK8sProjApplication.kt
@@ -0,0 +1,11 @@
+package com.g0dkar.samplek8sproj
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SampleK8sProjApplication
+
+fun main(args: Array<String>) {
+	runApplication<SampleK8sProjApplication>(*args)
+}
---
+++ src/test/kotlin/com/g0dkar/samplek8sproj/SampleK8sProjApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.g0dkar.samplek8sproj
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SampleK8sProjApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

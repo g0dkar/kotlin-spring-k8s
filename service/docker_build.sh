9bd17353a1ec28264f039693f03a41c23f3d397c
Rafael Lins <rafael.lins@n26.com>
2019-12-24 20:27:14 +0100

    Better dockerfile and build

--- service/docker_build.sh
+++ service/docker_build.sh
@@ -6,9 +6,9 @@ mvn clean dependency-check:check package -DfinalName=app -DskipTests
 
 echo "Building Image..."
 IMAGE=sample-k8s-service
-TAG=`git log --format="%H" -n 1 | cut -c1-6`
+TAG=$(git log --format="%H" -n 1 | cut -c1-6)
 
 docker build -t ${IMAGE}:${TAG} \
-             -t ${IMAGE}:latest .
+  -t ${IMAGE}:latest .
 
 rm -rf target/app.jar
